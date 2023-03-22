fcc153e0e971fc50ed7203fae87e384a2c4d4cbd
Cloves Paiva <cloves.paiva@extremedigital.com.br>
2023-03-22 15:15:44 -0300

    mock carrinho de compras

--- CarrinhoDeCompras/src/CarrinhoDeCompra.java
+++ CarrinhoDeCompras/src/CarrinhoDeCompra.java
@@ -4,9 +4,11 @@ import java.util.List;
 public class CarrinhoDeCompra {
 
 	private List<Produto> itens = new ArrayList<>();
+	private ObservadorCarrinho observer;
 	
 	public void adionaProduto(Produto p) {
 		itens.add(p);
+		this.observer.produtoAdicionado(p.getNome(), p.getValor());
 	}
 	
 	public int total() {
@@ -14,5 +16,9 @@ public class CarrinhoDeCompra {
 		for(Produto p : itens)
 			total += p.getValor();
 		return total;
+	}
+
+	public void adicionarObervador(ObservadorCarrinho observador) {
+		this.observer = observador;
 	}
 }
--- CarrinhoDeCompras/src/TesteCarrinhoCompras.java
+++ CarrinhoDeCompras/src/TesteCarrinhoCompras.java
@@ -7,10 +7,21 @@ class TesteCarrinhoCompras {
 	@Test
 	void totalCarrinho() {
 		CarrinhoDeCompra c = new CarrinhoDeCompra();
+		MockObservadorCarrinho mock = new MockObservadorCarrinho();
+		c.adicionarObervador(mock);
 		c.adionaProduto(new Produto("tenis", 100));
 		c.adionaProduto(new Produto("camieta", 50));
 		c.adionaProduto(new Produto("bermuda", 70));
 		assertEquals(220, c.total());
+	}
+	
+	@Test
+	void escutaAdicaoDeProduto() {
+		CarrinhoDeCompra c = new CarrinhoDeCompra();
+		MockObservadorCarrinho mock = new MockObservadorCarrinho();
+		c.adicionarObervador(mock);
+		c.adionaProduto(new Produto("tenis", 100));
+		mock.verificaRecebimentoProduto("tenis", 100);
 	}
 
 }
