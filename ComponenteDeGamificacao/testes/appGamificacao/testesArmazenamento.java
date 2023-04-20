b399d6c2e12c5be6eb998a415431b38a2424cf40
Cloves Paiva <cloves.paiva@extremedigital.com.br>
2023-04-20 20:10:43 -0300

    adding dependency for reading and write json file

--- ComponenteDeGamificacao/testes/appGamificacao/testesArmazenamento.java
+++ ComponenteDeGamificacao/testes/appGamificacao/testesArmazenamento.java
@@ -2,6 +2,16 @@ package appGamificacao;
 
 import static org.junit.Assert.*;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+ 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,7 +35,7 @@ public class testesArmazenamento {
 	@Test
 	public void metodoRecuperarPontosRetornaInteiroPositivo() {
 		assertEquals((Integer)5, armazenador.recuperarPontos("estrela", "Guerra"));
-		assertTrue((Integer)0 <= armazenador.recuperarPontos("estrela", "guerra"));
+		assertTrue((Integer)0 <= armazenador.recuperarPontos("estrela", "Guerra"));
 	}
 	
 	@Test
@@ -38,6 +48,31 @@ public class testesArmazenamento {
 	public void metodoTiposPontuacoesRegistradasRetornaListaDeTiposDePontuacaoRegistradas() {
 		List<String> pontuacoesRegistradas = Arrays.asList("estrela", "moeda", "curtida");
 		assertEquals(pontuacoesRegistradas, armazenador.tiposPontuacoesRegistradas());
+	}
+	
+	@Test
+	public void conteudoEstaSendoRegistradoEmArquivoJSON() throws FileNotFoundException, IOException, ParseException {
+		List<RegistroDePontuacao> pontuacoesRegistradas = this.lerArquivoJson();
+		assertEquals(pontuacoesRegistradas, armazenador.getRegistros());
+	}
+	
+	@SuppressWarnings("unused")
+	private List<RegistroDePontuacao> lerArquivoJson() throws FileNotFoundException, IOException, ParseException{
+		JSONParser parser = new JSONParser();
+		ArrayList<RegistroDePontuacao> json = (ArrayList) parser.parse(new FileReader("/home/cloves-paiva/Documentos/personal-projects/TDD/ComponenteDeGamificacao/src/appGamificacao/dadosDePontuacaoUsuario.json"));
+	    List<RegistroDePontuacao> result = new ArrayList<RegistroDePontuacao>();
+	    for(int i = 0 ; i < json.size() ; i++){
+//	    	JSONObject jsonObject = (JSONObject) json.get(i);
+//            String nomeUsuario = (String) jsonObject.get("nomeUsuario");
+//            String tipoDePontuacao = (String) jsonObject.get("tipoDePontuacao");
+//            Integer pontuacao = (Integer) jsonObject.get("pontuacao");
+//            RegistroDePontuacao registroPontuacao = new RegistroDePontuacao();
+//    		registroPontuacao.setTipoDePontuacao(tipoDePontuacao);
+//    		registroPontuacao.setPontuacao(pontuacao);
+//    		registroPontuacao.setNomeUsuario(nomeUsuario);
+	        result.add(json.get(i));
+	    }
+		return result;
 	}
 
 }
